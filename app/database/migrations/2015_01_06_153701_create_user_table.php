12580d3c6b8526b268f5137686be30fa1be7db71
tommyeap <ms.yeap91@gmail.com>
2015-01-09 17:57:15 +0800

    add in some fields to migration user table

--- app/database/migrations/2015_01_06_153701_create_user_table.php
+++ app/database/migrations/2015_01_06_153701_create_user_table.php
@@ -15,13 +15,16 @@ class CreateUserTable extends Migration {
 		Schema::create('users',function($table){
 			$table->increments('id');
 			$table->string('email')->unique();
-			$table->string('first_name');
-			$table->string('last_name');
+			$table->string('first_name',200);
+			$table->string('last_name',200);
 			$table->string('password');
+			$table->string('password_temp');
 			$table->bigInteger('uid_fb');
 			$table->string('birthday');
 			$table->string('photo_url');
 			$table->string('fb_token');
+			$table->boolean('active');
+			$table->string('activate_code',60);
 			$table->string('remember_token');
 			$table->timestamps();
 
