4c67d4d090934b69e478728b1617a7c0303b5238
tommyeap <ms.yeap91@gmail.com>
2015-01-12 17:01:50 +0800

    update level 2 favicon link

--- app/views/includes/head2.blade.php
+++ app/views/includes/head2.blade.php
@@ -6,7 +6,7 @@
 <meta name="wot-verification" content="0860b6923e18aef9b0b3"/> 
 <meta name="keywords" content="Ubinion, Ubi, nion, student, universiti, university, Community, past year, question, UUM, Universiti Utara Malaysia, Sintok, Kedah, Ubinion.com, Malaysia" />
 <meta name="author" content="Ubinion" />
-<link rel="shortcut icon" href="favicon.png">
+<link rel="shortcut icon" href="../favicon.png">
 <link rel="stylesheet" type="text/css" href="../css/ubinion_core.min.css"> 
 <link rel="stylesheet" type="text/css" href="../style.css" />	
 <link rel="stylesheet" type="text/css" href="../js/plugin/bootstrapvalidator/dist/css/bootstrapValidator.min.css" />	
