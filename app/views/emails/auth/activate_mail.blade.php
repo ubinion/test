dd798ff69ab47c1bbc77e3a596cecc02cf55a29e
rainings <kelvinrain@gmail.com>
2015-01-14 15:18:49 +0800

    Active Email Design and styling.

--- app/views/emails/auth/activate_mail.blade.php
+++ app/views/emails/auth/activate_mail.blade.php
@@ -1,19 +1,47 @@
-<!DOCTYPE html>
-<html lang="en-US">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
 	<head>
-		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Welcome to Ubinion!</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</head>
 	<body>
-		<h2>Greeting from Ubinion.com</h2>
-		<h3>Hi, {{ $name }} </h3>
-		<div>
-			Please activate your account using the following link<br><br>
-			----------<br>
-			{{ $link}}
-			----------<br>
-
-
-		</div>
-		
+		<table align="center" cellpadding="0" cellspacing="0" width="100%" style="border: 1px solid #cccccc;">
+			<tr>
+				<td bgcolor="#1a202c" style="padding: 25px 0 25px 0;" align="center">
+					<img src="../../../../public/img/logo/logo_email.png" alt="Logo_Ubinion" width="250px" height="61px"/>
+				</td>
+			</tr>
+			<tr>
+				<td bgcolor="#f3f3f3" style="padding: 40px 30px 40px 30px;">
+				 	<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: Arial, sans-serif; font-size: 17px; border: 1px solid #cccccc; background-color: #fff ">
+						<tr>
+						   	<td style="text-transform:uppercase; padding:15px 0 0 15px;">
+						   		Dear {{ $name }},
+						   	</td>
+						</tr>
+						<tr>
+						   	<td style="padding:15px 0 15px 15px;">
+						   		<strong>Thank you for registering on Ubinion.com!</strong>
+						   	</td>
+						</tr>										 		
+					  	<tr>
+						   <td style="font-family: Arial, sans-serif; font-size: 17px; padding:0 0 15px 15px;">
+						    	To activate your account, please click the link below.
+					  	</tr>
+					  	<tr>
+						   <td style="padding:0 0 15px 15px;">
+						    	{{ $link}}
+						   </td>
+					  	</tr>
+				 	</table>	
+				</td>
+			</tr>
+			<tr>
+			  	<td bgcolor="#f3f3f3" style="font-family: Arial, sans-serif; font-size: 15px; color:#ggg; padding:10px 0 10px 30px;">
+			   	Ubinion &copy; <?PHP echo date("Y"); ?> | <a href="https://www.facebook.com/ubinion" style="text-decoration:none"> Facebook</a>
+			  	</td>
+			</tr>
+		</table>	
 	</body>
 </html>
