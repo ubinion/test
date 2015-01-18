1f771a60f3f3c6af410de8896a5f1417488751c6
rainings <kelvinrain@gmail.com>
2015-01-18 22:42:29 +0800

    1) Remove the old redundancy of the meta tag.

--- app/views/emails/auth/forgot_mail.blade.php
+++ app/views/emails/auth/forgot_mail.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html, image/html; charset=UTF-8" />
+		<meta charset="utf-8" />
 		<title>It's ok, it happens. Here is yours...</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</head>
