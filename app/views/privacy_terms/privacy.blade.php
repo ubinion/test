0e44bf9b8db5dc9fc4413a791de1f719337f50f9
rainings <kelvinrain@gmail.com>
2015-02-05 21:18:04 +0800

    1) Updated Privacy Page

--- app/views/privacy_terms/privacy.blade.php
+++ app/views/privacy_terms/privacy.blade.php
@@ -7,20 +7,20 @@
 --}}
 
 @section('content')
-	<h1>Ubinion Privacy</h1>
+	<h1><strong>Ubinion Privacy</strong></h1>
 		<p>
-			Updated 4 February 2015
+			Updated <i>4 February 2015</i>
 		</p>
 		<h3>Information We Collect</h3>
-		<ol class="info text-justify">
-			<li class="info-row">
-				<h3>What You Directly Provide Us</h3>
-				<p class="text">
+		<ol class="text-justify">
+			<li>
+				<h3><strong>What You Directly Provide Us</strong></h3>
+				<p>
 					When you to create a Ubinion account, we need to collect a few basic details about you: including first name, last name, profile picture, gender, age range or date of birth and email address that we can contact you at. For additional details needed for Ubinion account, we may ask you for additional information such as work and education details and place you lived.  We want you, to be in control of your information, so we let you update or correct most of the basic Ubinion account information by editing your account settings within Ubinion.	
 				</p>
 			</li>
-			<li class="info-row">
-				<h3>How We Share Your Information.</h3>
+			<li>
+				<h3><strong>How We Share Your Information</strong></h3>
 				<p>
 					Ubinion is a student community, and the content you post may be shared with and viewable by others. You can choose to post the content by anonymous or your default public account. You cannot control who views your content that you share on Ubinion. So, if you don’t want to share something publicly by your public account you can choose to post with anonymous. When you choose to post by anonymous, your account information will remain anonymous and private. We do not identify you by name or contact information to other Ubinion users. All the content share by anonymous option will remain default by anonymous. However, we cannot prevent others from determining your identify from the content you posts or how you share content through third party sites.
 				</p>
@@ -28,38 +28,56 @@
 			<p>
 				The information we collect about you, including personally identifiable information, may be shared as follows:	
 			</p>
-			<p>
-				<strong>Ubinion Users (public option). </strong>
-				Any information that you post, including comments, replies and other information may become public. We cannot control how your information is used by others. Your posts are available to all other users. Users can also freely share any Ubinion content, no matter who posted it, though other third party sites like Facebook, Twitter and Instagram.	
-			</p>
-			<p>
-				<strong>Ubinion Users (anonymous option). </strong>
-				Any information that you post, including comments, replies and other information may become public. Your account information will remain private and anonymous. We cannot control how your information is used by others. Your posts are available to all other users. Users can also freely share any Ubinion content, no matter who posted it, though other third party sites like Facebook, Twitter and Instagram.
-			</p>
-			<!--p>
-				<strong>Service Providers. </strong>
-				We may disclose the information we collect from you to third party vendors, service providers, contractors or agents who perform functions on our behalf.
-			</p>
-			<p>
-				<strong>Business Transfers. </strong>
-				If we are acquired by or merged with another company, if substantially all of our assets are transferred to another company, or as part of a bankruptcy proceeding, we may transfer the information we have collected from you to the other company.
-			</p>
-			<p>
-				<strong>Third Parties and Business Partners. </strong>
-				We may share the information we gather about you and other users with our third party business partners, including advertisers. For example, if you post content that mentions one of our business partners, we may share that content with them. As noted, we don’t collect name or contact information (unless you post it in your comment or reply)
-			</p-->	
-			<p>
-				<strong>In Response to Legal Process. </strong>
-				We also may disclose the information we collect about you in order to comply with the law, a judicial proceeding, court order, subpoena, or other legal process. 
-			</p>
-			<p>
-				<strong>To Protect Us and Others. </strong>
-				We also may disclose the information we collect from you where we believe it is necessary to investigate, prevent, or take action regarding illegal activities, suspected fraud, situations involving potential threats to the safety of any person, violations of our Terms of Use or this Policy, or as evidence in litigation in which Ubinion is involved.
-			</p>
-			<!--p>
-				<strong>Aggregate and De-Identified Information. </strong>
-				We may also share aggregate or de-identified information about users with third parties for marketing, advertising, research or similar purposes
-			</p-->
+			<ul>
+				<li>
+					<p>
+						<strong>Ubinion Users (public option). </strong>
+						Any information that you post, including comments, replies and other information may become public. We cannot control how your information is used by others. Your posts are available to all other users. Users can also freely share any Ubinion content, no matter who posted it, though other third party sites like Facebook, Twitter and Instagram.	
+					</p>
+				</li>
+				<li>
+					<p>
+						<strong>Ubinion Users (anonymous option). </strong>
+						Any information that you post, including comments, replies and other information may become public. Your account information will remain private and anonymous. We cannot control how your information is used by others. Your posts are available to all other users. Users can also freely share any Ubinion content, no matter who posted it, though other third party sites like Facebook, Twitter and Instagram.
+					</p>
+				</li>
+				<!--li>	
+					<p>
+						<strong>Service Providers. </strong>
+						We may disclose the information we collect from you to third party vendors, service providers, contractors or agents who perform functions on our behalf.
+					</p>
+				</li-->
+				<!--li>
+					<p>
+						<strong>Business Transfers. </strong>
+						If we are acquired by or merged with another company, if substantially all of our assets are transferred to another company, or as part of a bankruptcy proceeding, we may transfer the information we have collected from you to the other company.
+					</p>
+				</li-->
+				<!--li>	
+					<p>
+						<strong>Third Parties and Business Partners. </strong>
+						We may share the information we gather about you and other users with our third party business partners, including advertisers. For example, if you post content that mentions one of our business partners, we may share that content with them. As noted, we don’t collect name or contact information (unless you post it in your comment or reply)
+					</p>
+				</li-->	
+				<li>	
+					<p>
+						<strong>In Response to Legal Process. </strong>
+						We also may disclose the information we collect about you in order to comply with the law, a judicial proceeding, court order, subpoena, or other legal process. 
+					</p>
+				</li>
+				<li>
+					<p>
+						<strong>To Protect Us and Others. </strong>
+						We also may disclose the information we collect from you where we believe it is necessary to investigate, prevent, or take action regarding illegal activities, suspected fraud, situations involving potential threats to the safety of any person, violations of our Terms of Use or this Policy, or as evidence in litigation in which Ubinion is involved.
+					</p>
+				</li>
+				<!--li>
+					<p>
+						<strong>Aggregate and De-Identified Information. </strong>
+						We may also share aggregate or de-identified information about users with third parties for marketing, advertising, research or similar purposes
+					</p>
+				</li-->
+			</ul>
 			<p>
 				You must be at least <strong>17</strong> to use our Services. Our Services are not designed for children under <strong>13</strong>. If we discover that a child under 13 has provided us with personal information, we will delete such information from our systems.
 				Contact Us
@@ -67,11 +85,11 @@
 			</p>
 				If you have questions about the privacy aspects of our Services or would like to make a complaint, please contact us at <a href="#">support@ubinion.com</a>
 			</p>
-			<li class="info-row">
-				<h3>Changes to this Policy</h3>
+			<li>
+				<h3><strong>Changes to this Policy</strong></h3>
 			</li>
 			<p>
-				This Policy is current as of the Effective Date set forth above. We may change this Policy from time to time, so please be sure to check back periodically. We will post any changes to this Policy on our Site, at <a href="http://www.ubinion.com/privacy_terms/privacy">Ubinion Privacy</a>. If we make any changes to this Policy that materially affect our practices with regard to the personal information we have previously collected from you, we will endeavor to provide you with notice in advance of such change by highlighting the change on our Site.
+				This Policy is current as of the Effective Date set forth above. We may change this Policy from time to time, so please be sure to check back periodically. We will post any changes to this Policy on our Site, at <a href="{{ URL::route('privacy-page')}}">Ubinion Privacy</a>. If we make any changes to this Policy that materially affect our practices with regard to the personal information we have previously collected from you, we will endeavor to provide you with notice in advance of such change by highlighting the change on our Site.
 			</p>			
 		</ol>	
 @stop
